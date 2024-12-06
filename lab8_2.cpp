bb8ad449648721c48e3815ae90b3a54ce44c3512
YoYaYee <eden555007@gmail.com>
2024-12-06 15:13:09 +0700

    lab8_2_complete.cpp

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -35,7 +35,7 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
-		getline(cin,name[i]);
+		getline (cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
 		cin >> score[i];
